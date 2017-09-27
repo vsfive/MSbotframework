334bd31e067eeaa880a58895b0d859257ed19d01
Vinit Shandilya <vinitsfive@gmail.com>
2017-09-27 10:01:28 -0400

    Working sample bot

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '7a7e61ce-b562-4f06-9da9-2b3b82d3436f',
+    appPassword: 'WF2hGAa4LePZoeNfAX5yeiQ'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
